81d95a68fa0178de8ee4e014eb6e02bd54ebd7b2
triglan <glanb4277@gmail.com>
2025-11-11 13:47:42 +0900

    내가만든 격자무늬

--- SimpleGame/Shaders/FS.fs
+++ SimpleGame/Shaders/FS.fs
@@ -9,8 +9,13 @@ const float c_PI = 3.141592;
 void main()
 {
 	vec4 newColor = vec4(0);
-	float xValue= 1 - pow(abs(sin(v_UV.x * 2 * c_PI * 4)), 0.5);
+	//float xValue= pow(abs(sin(v_UV.x * 2 * c_PI * 4)), 0.5);
+	float xValue= abs(sin(v_UV.x * 2 * c_PI * 4));
+	float yValue= abs(sin(v_UV.y * 2 * c_PI * 4));
 	
-	newColor = vec4(xValue);
+	float merge = xValue * yValue;
+	
+	vec3 vec3Color = vec3(merge);
+	newColor = vec4(vec3Color, 1);
 	FragColor = newColor;
 }
