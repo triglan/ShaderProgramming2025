4c940a046dec43c3722f11d5d461bc4ddc37e486
triglan <glanb4277@gmail.com>
2025-11-11 13:23:28 +0900

    pow를 활용해 기둥 얇게 만들기

--- SimpleGame/Shaders/FS.fs
+++ SimpleGame/Shaders/FS.fs
@@ -9,7 +9,7 @@ const float c_PI = 3.141592;
 void main()
 {
 	vec4 newColor = vec4(0);
-	float xValue= abs(sin(x_UV.x * 2 * c_PI * 4));
+	float xValue= pow(abs(sin(x_UV.x * 2 * c_PI * 4)), 12);
 	newColor = vec4(xValue);
 
 	FragColor = newColor;
